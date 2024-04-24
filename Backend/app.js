780dfa2de3150ac424470b5fa697845266ba0e85
Ravindu Hirusanka <puresoftlk@gmail.com>
2024-04-24 15:20:29 +0530

    Add files via upload

---
+++ Backend/app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
+
+app.use(cors());
+app.options('*', cors());
+
+//middleware
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+//app.use(authJwt());
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+//app.use(errorHandler);
+
+
+//Routes
+const categoriesRoutes = require('./routers/categories');
+const productsRoutes = require('./routers/products');
+const usersRoutes = require('./routers/users');
+const ordersRoutes = require('./routers/orders');
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+
+
+
+//database
+mongoose.connect(process.env.CONNECTION_STRING, {
+   
+    dbName: 'eshop'
+})
+.then(()=>{
+    console.log('Database Connection is ready...')
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+const port =5000;
+
+//Server
+app.listen(port, ()=>{
+   
+    console.log(`Server is running now http://localhost:${port}`)
+})
+
+//npm init
+//npm install nodemon
+//npm start
+//npm install express
+//npm install dotenv
+//npm install body-parser
+//npm install morgan
+//npm install mongoose
+//npm install cors
+//npm install bcryptjs
+// npm i jsonwebtoken
+//npm i express-jwt
+//npm install multer
---
+++ Backend/routers/categories.js
@@ -0,0 +1,20 @@
+
+
+//Get a Category
+
+
+
+//Get a Category By Id
+
+
+
+//Post new Category
+
+
+
+//Update a Category
+
+
+
+
+
