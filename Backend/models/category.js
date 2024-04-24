73600298a1d93a8bf84a80d1f0a64439d2ccac85
LayuruLK <139636618+LayuruLK@users.noreply.github.com>
2024-04-24 19:43:28 +0530

    Update category.js
    This code segment defines a virtual field id for the categorySchema schema. The id field is generated by converting the _id field to a hexadecimal string. Additionally, it sets the toJSON option to include virtuals, ensuring that the virtual id field is included when converting the schema instance to JSON.
    
    As well as I entered export part.

--- Backend/models/category.js
+++ Backend/models/category.js
@@ -15,3 +15,13 @@ const categorySchema = mongoose.Schema({
     }
 
 })
+
+categorySchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+categorySchema.set('toJSON', {
+    virtuals: true,
+});
+
+exports.Category = mongoose.model('Category', categorySchema);
