3a8668c0d232d408b85df238500e23d1936452a4
LayuruLK <139636618+LayuruLK@users.noreply.github.com>
2024-04-26 07:09:39 +0530

    Update categories.js
    Express.js routes for managing categories in the web application. Includes endpoints for retrieving all categories and fetching a category by ID.

--- Backend/routers/categories.js
+++ Backend/routers/categories.js
@@ -1,20 +1,25 @@
+const {Category} = require('../models/category');
+const express = require('express');
+const router = express.Router();
 
 
 //Get a Category
+router.get('/', async (req, res)=>{
+    const categoryList = await Category.find();
 
+    if(!categoryList){
+        res.status(500).json({success: false})
+    }
+    res.status(200).send(categoryList);
+})
 
 
 //Get a Category By Id
+router.get('/:id', async (req, res)=>{
+    const category =await Category.findById(req.params.id);
 
-
-
-//Post new Category
-
-
-
-//Update a Category
-
-
-
-
-
+    if(!category){
+        res.status(500).json({message: 'The category with given ID was not available!'});
+    }
+    res.status(200).send(category);
+})
