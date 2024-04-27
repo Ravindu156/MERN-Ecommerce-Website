3ede2927cc0358e473d2a1f63653f46e3a9369f9
Ravindu Hirusanka <puresoftlk@gmail.com>
2024-04-27 23:14:38 +0530

    Update categories.js
    Delete f

--- Backend/routers/categories.js
+++ Backend/routers/categories.js
@@ -58,4 +58,19 @@ router.put('/:id', async (req, res)=>{
     }
     res.send(category);
 })
+//delete
+router.delete('/:id', (req, res)=>{
+    Category.findByIdAndDelete(req.params.id).then(category =>{
+        if(category){
+            return res.status(200).json({success: true, message: 'The category is deleted successfully!'})
+        }
+        else{
+            return res.status(404).json({success: false, message: 'Category not found!'})
+        }
+    }).catch(err=>{
+        return res.status(500).json({success: false, error: err})
+    })
+})
+
+module.exports = router;
 
